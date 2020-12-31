e83d7704349cfc847d056d0ed5e1c97d225b026c
Link717 <ksy95433@gmail.com>
2020-12-31 23:59:36 +0900

    Modify: 2020-12-31 codekata

--- KakaoBlindRecruitment/songJustNow.js
+++ KakaoBlindRecruitment/songJustNow.js
@@ -1,39 +1,35 @@
-const conversion = (musicinfos) => {
-  let splitStr = musicinfos.map(el => el.split(","));
-  let runNotes = {};
-  
-  for (let music of splitStr) {
-    let runtimes = (music[1].split(":")[1] - music[0].split(":")[1]);
-    let notes = music[3];
-    
-    if (notes.length >= runtimes) {
-      runNotes[music[2]] = notes.slice(0, runtimes);
-    } else {
-      if (runtimes%(notes.length) === 0) {
-        let cycles = runtimes/(notes.length);
-        let str = "";
-        for (let i = 0; i < cycles-1; i++) {
-          notes += notes;
-        }
-        runNotes[music[2]] = notes;
-      } else {
-        let cycles = Math.ceil(runtimes/(notes.length));
-        let restNotes = runtimes%(notes.length);
-          for (let i = 0; i < cycles-1; i++) {
-          notes += notes;
-        }
-        notes += notes.slice(0, restNotes);
-        runNotes[music[2]] = notes;
-      }
-    }
-  }
-  return runNotes;
+// sharp 변환
+const changeSharp = (str) => {
+  let _str = str.replace(/C#/g, "c").replace(/D#/g, "d").replace(/F#/g, "f").replace(/G#/g, "g").replace(/A#/g, "a")
+  return _str;
 }
 
+// 시간 계산
+const conversionTime = (start, end) => {
+  return ((parseInt(end.substring(0,2)) - parseInt(start.substring(0,2))) * 60 + 
+  (parseInt(end.substring(3)) - parseInt(start.substring(3))))  
+}
+
+
 const solution = (m, musicinfos) => {
-  let convNotes = conversion(musicinfos);
-  let booleanArr = Object.entries(convNotes).map(el => el[1].includes(m));
-  for (let j = 0; j < booleanArr.length; j++) {
-    if (booleanArr[j]) return Object.keys(convNotes)[j];
+  let _m = changeSharp(m);
+  let stack = [];
+  
+  // 재생시간에 따른 제목, 음계 정보
+  for (let music of musicinfos) {
+    let [start, end, title, notes] = music.split(",");
+    let timeDiff = conversionTime(start, end);
+    let _notes = changeSharp(notes);
+    if (timeDiff <= _notes.length) {
+      stack.push([title, _notes.slice(0, _m.length)]);
+    } else {
+      let cycles = Math.floor(timeDiff/_notes.length);
+      let restNotes = timeDiff%_notes.length;
+      stack.push([title, _notes.repeat(cycles) + _notes.slice(0, restNotes)])
+    }
   }
+  
+  // stack에 담긴 노래 중 조건부 음악 출력
+
+  stack.map(el => console.log(el[1]));
 }
