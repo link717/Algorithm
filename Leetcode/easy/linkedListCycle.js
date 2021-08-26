b5e309bc44aa81c26d7f6a6e7c2a95b452ae1566
Suyeon Kim <ksy95433@gmail.com>
2021-08-26 23:04:49 +0900

    Add: 2021-08-26 codekata

---
+++ Leetcode/easy/linkedListCycle.js
@@ -0,0 +1,46 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+
+/*
+  Definition for singly-linked list.
+  function ListNode(val) {
+      this.val = val;
+      this.next = null;
+  }
+ */
+
+/*
+  @param {ListNode} head
+  @return {boolean}
+ */
+
+/*
+트랙을 뛰는 선수 a, b를 상상해보자.
+트랙이 직선 코스면 선수 a, b는 만나지 않는다.
+트랙이 둥근 코스면 선수 a, b는 만나게 된다.
+*/
+
+var hasCycle = function (head) {
+  let slowRunner = head;
+  let fastRunner = head;
+
+  if (head == null) return false;
+
+  if (head.next == null) return false;
+
+  while (fastRunner.next != null && fastRunner.next.next != null) {
+    slowRunner = slowRunner.next;
+    fastRunner = fastRunner.next.next;
+
+    if (slowRunner == fastRunner) {
+      return true;
+    }
+  }
+  return false;
+};
