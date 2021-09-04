d1971ae24b914c659ead113300381787ae932340
Suyeon Kim <ksy95433@gmail.com>
2021-09-04 22:08:31 +0900

    Add: 2021-09-04 codekata

---
+++ Programmers/Lv.2/카펫.js
@@ -0,0 +1,22 @@
+function solution(brown, yellow) {
+  // 가로 >= 세로 길이이므로 가장 크게 가질 수 있는 가로 길이를 설정
+  const maxWidth = brown + yellow;
+
+  // (width * height) = brown + yellow를 참조하여 완전 탐색 루프 실행
+  for (let i = maxWidth; i > 0; i--) {
+    if (maxWidth % i == 0) {
+      let tmpWidth = i;
+      let tmpHeight = maxWidth / i;
+
+      if (checkSize(tmpWidth, tmpHeight)) return [tmpWidth, tmpHeight];
+    }
+  }
+
+  // 입력된 가로, 세로 값으로 유효성 검사하는 로직
+  function checkSize(width, height) {
+    let condition1 = (width + height) * 2 - 4 == brown;
+    let condition2 = width * height - brown == yellow;
+
+    return condition1 && condition2;
+  }
+}
